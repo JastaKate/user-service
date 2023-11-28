aa0b4387a4fbccf485c983519b21c6ee3a9f0d94
Kate <106038032+JastaKate@users.noreply.github.com>
2023-11-28 21:20:27 +0300

    done

--- src/main/java/com/example/userservice1/threads/ThreadLocalPayload.java
+++ src/main/java/com/example/userservice1/threads/ThreadLocalPayload.java
@@ -2,18 +2,18 @@ package com.example.userservice1.threads;
 
 public class ThreadLocalPayload {
 
-    private static final ThreadLocal<String> CONTEXT = new ThreadLocal<>();
+    private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
     public static void setId(String tenantId) {
-        CONTEXT.set(tenantId);
+        threadLocal.set(tenantId);
     }
 
     public static String getId() {
-        return CONTEXT.get();
+        return threadLocal.get();
     }
 
     public static void clear() {
-        CONTEXT.remove();
+        threadLocal.remove();
     }
 
 }
